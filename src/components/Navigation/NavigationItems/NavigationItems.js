1377f551dbdee4882b58cebab0e504c822b6ad75
gunaalan <elangogunaalan@gmail.com>
2021-02-28 23:23:54 +0530

    Styles to nav bar and its items

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavigationItem from "./NavigationItem/NavigationItem";
+import classes from "./NavigationItems.module.css";
+
+const navigationItems = (props) => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" active>
+      BurgerBuilder
+    </NavigationItem>
+    <NavigationItem link="/">Order</NavigationItem>
+  </ul>
+);
+
+export default navigationItems;
