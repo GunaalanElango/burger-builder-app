1a709a22497ed64dfd26f9617e7ecaeace8f96f0
gunaalan <elangogunaalan@gmail.com>
2021-03-01 23:55:20 +0530

    Created spinner component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -7,7 +7,10 @@ class Modal extends Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.showModal !== this.props.showModal;
+    return (
+      nextProps.showModal !== this.props.showModal ||
+      nextProps.children !== this.props.children
+    );
   }
 
   render() {
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import Backdrop from "../../components/UI/Backdrop/Backdrop";
+import Spinner from "../../components/UI/Spinner/Spinner";
 import axios from "../../axios-orders";
 
 let INGREDIENT_PRICES = {
@@ -31,6 +32,7 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 50,
     showModal: false,
+    loading: false,
   };
 
   addIngredientHandler = (type) => {
@@ -73,6 +75,7 @@ class BurgerBuilder extends Component {
   };
 
   continueCheckoutHandler = () => {
+    this.setState({ loading: true });
     const order = {
       ingredients: this.state.ingredients,
       totalPrice: this.state.totalPrice,
@@ -91,8 +94,8 @@ class BurgerBuilder extends Component {
     };
     axios
       .post("/orders.json", order)
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));
+      .then((response) => this.setState({ loading: false, showModal: false }))
+      .catch((error) => this.setState({ loading: false, showModal: false }));
   };
 
   render() {
@@ -117,17 +120,23 @@ class BurgerBuilder extends Component {
 
     const orderDisable = !orderBtnDisable.includes(false, 0);
 
+    let orderSummary = (
+      <OrderSummary
+        ingredients={this.state.ingredients}
+        cancel={this.backdropHandler}
+        continue={this.continueCheckoutHandler}
+        price={this.state.totalPrice}
+      />
+    );
+
+    if (this.state.loading) {
+      orderSummary = <Spinner />;
+    }
+
     return (
       <Aux>
         <Backdrop show={this.state.showModal} click={this.backdropHandler} />
-        <Modal showModal={this.state.showModal}>
-          <OrderSummary
-            ingredients={this.state.ingredients}
-            cancel={this.backdropHandler}
-            continue={this.continueCheckoutHandler}
-            price={this.state.totalPrice}
-          />
-        </Modal>
+        <Modal showModal={this.state.showModal}>{orderSummary}</Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           addIngredient={this.addIngredientHandler}
