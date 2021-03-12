1a33a938324f3c4a1f46905e6956c0cd35c4f9d7
gunaalan <elangogunaalan@gmail.com>
2021-03-12 11:32:09 +0530

    Adding Orders page

---
+++ src/components/Burger/Order/Order.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./Order.module.css";
+
+const order = (props) => {
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: Salad (1)</p>
+      <p>
+        Price: <strong>34 Rupess</strong>
+      </p>
+    </div>
+  );
+};
+
+export default order;
